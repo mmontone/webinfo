e67ee4bca0ccd525f969a8d612bab723e254683f
Mariano Montone <marianomontone@gmail.com>
2021-04-04 21:26:10 -0300

    Sketches

--- html.lisp
+++ html.lisp
@@ -4,3 +4,8 @@
   ()
   (:documentation "An info document that renders the content of an HTML file.
 Also makes indexes from HTML headings"))
+
+(defparameter *hunch-html*
+  (plump:parse (asdf:system-relative-pathname :hunchentoot "docs/index.html")))
+
+
