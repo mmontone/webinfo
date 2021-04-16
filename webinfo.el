37fffeb697d58d11de690f9c851539b3a4cab10c
Mariano Montone <marianomontone@gmail.com>
2021-04-15 23:07:33 -0300

    Starting an Emacs mode

--- webinfo.el
+++ webinfo.el
@@ -30,3 +30,21 @@
     ('eww (eww-browse-url (concat url "&_c=eww")))
     ('browse-url (browse-url (concat url "&_c=browse-url")))
     (t (eww-browse-url url))))
+
+(defvar webinfo-keymap
+  (let ((map (make-sparse-keymap "webinfo")))
+    (define-key map "t" 'webinfo-navigate-top)
+    (define-key map "u" 'webinfo-navigate-up)
+    (define-key map "n" 'webinfo-navigate-next)
+    (define-key map "p" 'webinfo-navigate-previous)
+    (define-key map "h" 'webinfo-help)
+    (define-key map "s" 'webinfo-search)
+    (define-key map "l" 'webinfo-navigate-back)
+    map))
+
+(define-minor-mode webinfo-mode
+  "WebInfo minor mode."
+  :init-value nil
+  :lighter " WebInfo"
+  :keymap webinfo-keymap
+  :group 'webinfo)
