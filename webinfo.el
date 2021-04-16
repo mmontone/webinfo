ccce8c68b146b9ab968c209d3622ee03ea2f17a0
Mariano Montone <marianomontone@gmail.com>
2021-04-15 22:09:12 -0300

    emacs: customizations

--- webinfo.el
+++ webinfo.el
@@ -1,9 +1,25 @@
 (require 'browse-url)
+(require 'eww)
 
-(defvar webinfo-url "http://localhost:9090")
-(defvar webinfo-browser 'eww)
+(defgroup webinfo ()
+  "WebInfo customizations"
+  :prefix "webinfo-")
+
+(defcustom webinfo-url "http://localhost:9090"
+  "The url of the WebInfo service"
+  :type 'string
+  :group 'webinfo)
+
+(defcustom webinfo-browser 'eww
+  "WebInfo browsing method"
+  :type 'symbol
+  :group 'webinfo)
 
 (defun webinfo-apropos-symbol (symbol)
+  (interactive "sSymbol:")
+  (webinfo-browse-url (format "%s/_s?q=%s" webinfo-url symbol)))
+
+(defun webinfo-lookup-symbol (symbol)
   (interactive "sSymbol:")
   (webinfo-browse-url (format "%s/_s?q=%s" webinfo-url symbol)))
 
