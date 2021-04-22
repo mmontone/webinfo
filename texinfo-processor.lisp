29153993ab983b462b05e95eccbf6276fa590233
Mariano Montone <marianomontone@gmail.com>
2021-04-22 01:14:40 -0300

    Idea

--- texinfo-processor.lisp
+++ texinfo-processor.lisp
@@ -9,5 +9,6 @@
 (defun process-texinfo-file (file)
   "Expands @clfunction, @clmacro, etc. definitions to a Texinfo definition with body extracted from Common Lisp code")
 
+;; an idea could be to use the following via an @clpackage macro that expands to a full Texinfo chapter with definitions
 (defun generate-texinfo-reference-for-package (package stream)
   "Generates a Texinfo reference with PACKAGE external symbols documentation")
