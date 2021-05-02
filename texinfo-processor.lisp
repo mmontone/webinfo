19cca8ac5a3aef4cfe998967a79301c632f54830
Mariano Montone <marianomontone@gmail.com>
2021-05-02 10:56:02 -0300

    texinfo-processor: work in progress

--- texinfo-processor.lisp
+++ texinfo-processor.lisp
@@ -14,7 +14,7 @@
   '((:@clfunction . "@clfunction{(.*):(.*)}")))
 
 (defun process-texinfo-file (file stream)
-  "Expands @clfunction, @clmacro, etc. definitions to a Texinfo definition with body extracted from Common Lisp code"
+  "Expands @clfunction, @clmacro, etc. definitions to a Texinfo definition with body extracted from Common Lisp code."
   (with-open-file (f file :direction :input
 			  :external-format :utf-8)
     (loop for line := (read-line f nil nil)
@@ -41,12 +41,20 @@
 			  (terpri stream))))))
 	       (t (write-string line stream)
 		  (terpri stream))))))
+
+;; @clpackage-functions
+;; @clpackage-variables
+;; @clpackage-classes
+
+;; TODO: make @cldefun reference source code when enabled (use swank location).
+;; Source code enabled is indicated with a Texinfo variable.
+;; Source code is serialized to a Texinfo node with an anchor for each line @anchor{<filename>L<linename>}
 		 
 
-;; an idea could be to use the following via an @clpackage macro that expands to a full Texinfo chapter with definitions
+;; an idea could be to use the following via an @clpackage-reference{package-name} macro that expands to a full Texinfo chapter with definitions
 (defun generate-texinfo-reference-for-package (package stream)
   "Generates a Texinfo reference with PACKAGE external symbols documentation")
 
 (with-output-to-string (s)
-  (process-texinfo-file (asdf:system-relative-pathname :webinfo "test/texinfo.texi")
-			s))
+  (process-texinfo-file
+   (asdf:system-relative-pathname :webinfo "test/texinfo.texi") s))
